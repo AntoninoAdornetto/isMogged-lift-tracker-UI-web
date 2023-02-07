dc5dbcf87614378ae7d1c4286ad65d779d5b3156
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2023-02-07 02:07:42 -0500

    [ADD] singleton

---
+++ src/lib/axios/index.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosRequestConfig } from "axios";
+
+class AxiosSingleton {
+  private static instance: AxiosSingleton;
+  private axios = axios.create({
+    baseURL: "http://localhost:8080",
+    withCredentials: true,
+  });
+
+  public static getInstance(): AxiosSingleton {
+    if (!AxiosSingleton.instance) {
+      AxiosSingleton.instance = new AxiosSingleton();
+    }
+    return AxiosSingleton.instance;
+  }
+
+  public async request(config: AxiosRequestConfig) {
+    return this.axios.request(config);
+  }
+}
+
+export default AxiosSingleton.getInstance();
