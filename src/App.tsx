a4a0899f5b9ee94e1b482617471128c8b34dd1c0
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2023-02-07 02:06:11 -0500

    [ADD] check if user has active session

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { useQuery } from "react-query";
+import renewToken from "@services/auth/renewToken";
+
+import { type APIError } from "@src/@types/api";
+import Navigation from "@layouts/navigation";
 
 function App() {
-  return <div>isMogged</div>;
+  const { data } = useQuery("session", renewToken, {
+    retry: false,
+    onError(err) {
+      const error = err as APIError;
+      console.error(error.response?.data.error);
+      return error.response?.data.error;
+    },
+  });
+
+  if (data?.user_id) {
+    return <Navigation userID={data.user_id} />;
+  }
+
+  return <div data-testid='home--page'>Home Page</div>;
 }
 
 export default App;
