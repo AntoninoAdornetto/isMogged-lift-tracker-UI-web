66beab20a049441142077b7bdb72fade153b094a
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2023-02-07 02:07:17 -0500

    [ADD] init router & styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+import { router } from "@lib/react-router/router";
+import "primereact/resources/themes/bootstrap4-dark-purple/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "@assets/css/main.css";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
