37a1b31377b7848be4b40cada6e406131083a5ce
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2023-02-07 23:48:44 -0500

    [ADD] JSX for home page

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Card } from "primereact/card";
+import { Button } from "primereact/button";
+
+import Container from "@layouts/container";
+import "./index.css";
+
+const Home: React.FC = () => {
+  const navigate = useNavigate();
+
+  const footer = (
+    <div className='flex justify-around'>
+      <Button label='Register' className='w-1/3' onClick={() => navigate("register")} />
+      <Button label='Login' className='w-1/3' onClick={() => navigate("login")} />
+    </div>
+  );
+
+  return (
+    <Container className='hero--container'>
+      <div className='flex justify-center items-center h-full w-full'>
+        <Card className='home--card mr-5 ml-5' footer={footer} title='isMogged'>
+          <p className='m-0'>
+            Pull up your huggies, grab your oven mitt and get ready to set some PRs
+          </p>
+        </Card>
+      </div>
+    </Container>
+  );
+};
+
+export default Home;
