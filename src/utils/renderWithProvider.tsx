2b64f7dbc154268654b56f2de454ac414293c970
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2023-02-07 19:06:56 -0500

    [EDIT] test components that use routes

--- src/utils/renderWithProvider.tsx
+++ src/utils/renderWithProvider.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { MemoryRouter } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
 export default function renderWithProvider(children: React.ReactNode) {
-  render(<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>);
+  render(
+    <MemoryRouter>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </MemoryRouter>
+  );
 }
