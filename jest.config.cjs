b7ba8a4fee47c62af6f4f11e870753976d752582
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2023-02-07 23:41:05 -0500

    [EDIT] alias's not used anymore

--- jest.config.cjs
+++ jest.config.cjs
@@ -9,8 +9,6 @@ module.exports = {
     "^@src(.*)$": "<rootDir>/src/$1",
     "^@assets(.*)$": "<rootDir>/src/assets/$1",
     "^@components(.*)$": "<rootDir>/src/components/$1",
-    "^@context(.*)$": "<rootDir>/src/context/$1",
-    "^@data(.*)$": "<rootDir>/src/data/$1",
     "^@lib(.*)$": "<rootDir>/src/lib/$1",
     "^@layouts(.*)$": "<rootDir>/src/layouts/$1",
     "^@pages(.*)$": "<rootDir>/src/pages/$1",
