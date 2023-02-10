865e638520ce6357b1c60180f1a249aeb9f0d1e2
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2023-02-09 23:49:36 -0500

    [EDIT] redirect cb as prop

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,25 +1,36 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import { Card } from "primereact/card";
 import { Button } from "primereact/button";
 
 import Container from "@layouts/container";
 import "./index.css";
 
-const Home: React.FC = () => {
-  const navigate = useNavigate();
+type HomeProps = {
+  handleRedirect: (route: "/login" | "/register") => void;
+};
 
+const Home: React.FC<HomeProps> = ({ handleRedirect }) => {
   const footer = (
     <div className='flex justify-around'>
-      <Button label='Register' className='w-1/3' onClick={() => navigate("register")} />
-      <Button label='Login' className='w-1/3' onClick={() => navigate("login")} />
+      <Button
+        data-testid='registerBtn'
+        label='Register'
+        className='w-1/3'
+        onClick={() => handleRedirect("/register")}
+      />
+      <Button
+        data-testid='loginBtn'
+        label='Login'
+        className='w-1/3'
+        onClick={() => handleRedirect("/login")}
+      />
     </div>
   );
 
   return (
-    <Container className='hero--container'>
+    <Container className='hero--container' id='home--page'>
       <div className='flex justify-center items-center h-full'>
-        <Card className='mr-5 ml-5' footer={footer} title='Ready to mog?'>
+        <Card className='mr-5 ml-5' footer={footer} title='Ready to Mog?'>
           <p className='m-0'>
             Pull up your huggies, grab your oven mitt and get ready to set some PRs
           </p>
