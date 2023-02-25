476651d3fed718e34fb51b9d7d3d5a3d2befc747
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2023-02-25 18:26:44 -0500

    [EDIT] decouple header from table

---
+++ src/components/ExerciseTable/Header.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { useQuery } from "react-query";
+import { Accordion, AccordionTab } from "primereact/accordion";
+import { InputText } from "primereact/inputtext";
+import { Dropdown, DropdownChangeParams } from "primereact/dropdown";
+
+import { listMuscleGroups } from "@services/muscle_group/listMuscleGroups";
+import AddExerciseForm from "@src/pages/exercise/forms/AddExercise";
+
+type HeaderProps = {
+  filteredMuscleGroup: string;
+  keyword: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+  onMuscleGroupChange: (e: DropdownChangeParams) => void;
+};
+
+export function Header({
+  filteredMuscleGroup,
+  keyword,
+  onChange,
+  onMuscleGroupChange,
+}: HeaderProps) {
+  const { data: muscleGroups } = useQuery("listMuscleGroups", listMuscleGroups, {
+    enabled: false,
+  });
+
+  return (
+    <div data-testid='exercisesTableHeader'>
+      <Accordion>
+        <AccordionTab header='Filter Options'>
+          <div>
+            <span className='p-input-icon-left mb-5 w-full'>
+              <i className='pi pi-search' />
+              <InputText
+                className='w-full'
+                data-testid='exerciseFilterInput'
+                placeholder='Search by exercise name'
+                onChange={onChange}
+                value={keyword}
+              />
+            </span>
+            {muscleGroups && (
+              <Dropdown
+                className='w-full'
+                data-testid='filterByMuscleGroupDropdown'
+                id='muscleGroup'
+                name='muscleGroup'
+                placeholder='Filter by muscle group'
+                onChange={onMuscleGroupChange}
+                options={[{ name: "All" }, ...muscleGroups]}
+                optionLabel='name'
+                optionValue='name'
+                value={filteredMuscleGroup}
+              />
+            )}
+          </div>
+        </AccordionTab>
+        <AccordionTab header='Create'>
+          <AddExerciseForm />
+        </AccordionTab>
+      </Accordion>
+    </div>
+  );
+}
