7f1038dac5810ca4190d46ecc9d8106bbc4fc914
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2023-02-28 19:48:16 -0500

    [ADD] init dashboard unit tests

--- src/pages/dashboard/index.spec.tsx
+++ src/pages/dashboard/index.spec.tsx
@@ -1,14 +1,32 @@
 import React from "react";
-import { screen, fireEvent } from "@testing-library/react";
+import Cookies from "js-cookie";
+import { screen } from "@testing-library/react";
 
 import renderWithProvider from "@utils/renderWithProvider";
+import Dashboard from ".";
+
+jest.mock("js-cookie", () => ({
+  default: {
+    get: jest.fn().mockImplementation(() => "mockUserId"),
+  },
+}));
+
+const cookieSpy = jest.spyOn(Cookies, "get");
 
 function init() {
-  renderWithProvider();
+  renderWithProvider(<Dashboard />);
 }
 
 describe("Dashboard page", () => {
+  beforeEach(jest.resetAllMocks);
+
   test("Dashboard page renders", () => {
-    screen.getByText("Mog Mode");
+    init();
+    screen.getByTestId("dashboardPage");
+  });
+
+  test("user_id cookie is retrieved", () => {
+    init();
+    expect(cookieSpy).toHaveBeenCalledTimes(1);
   });
 });
