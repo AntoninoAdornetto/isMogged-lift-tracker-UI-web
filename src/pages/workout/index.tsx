d26d626a0bbb98bfd99c03d4b1928ba66267ffed
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2023-03-03 21:05:12 -0500

    init workout page JSX

--- src/pages/workout/index.tsx
+++ src/pages/workout/index.tsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { Dialog } from "primereact/dialog";
+import { Button } from "primereact/button";
+
+import { WorkoutForm } from "./forms/WorkoutForm";
 
 export default function Workout() {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleNewWorkoutClick = async () => {
+    setIsActive(true);
+  };
+
+  const handleHide = () => {
+    setIsActive(false);
+  };
+
   return (
-    <div>
-      New Workout page here
-      <span>Get ready to mog son!</span>
+    <div data-testid='workoutPage'>
+      <div>Start workout from template?</div>
+      <div>
+        <Button label='Start from scratch' onClick={handleNewWorkoutClick} />
+      </div>
+      <Dialog onHide={handleHide} visible={isActive} position='top'>
+        <WorkoutForm />
+      </Dialog>
     </div>
   );
 }
